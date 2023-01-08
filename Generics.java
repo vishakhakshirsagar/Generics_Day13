f1738ed07ec874e27b705bd95bdf6c852c613ac0
vishakhakshirsagar <kshirsagarvishakha22@gmail.com>
2023-01-08 22:12:55 +0530

    Added: Find the Maximum

--- Generics.java
+++ Generics.java
@@ -1,7 +1,6 @@
 package Day13Assignmnet;
 
 public class Generics {
-        //compareTo method for Checking Maximum from 3 Integer Object
         public static Integer compareTo(Integer a,Integer b,Integer c) {
             if(a > b){
                 if(a > c) {
@@ -17,11 +16,27 @@ public class Generics {
                 }
             }
         }
-
+        //compareTo method for Checking Maximum from 3 Float Object
+        public static Float compareTo(Float a,Float b,Float c) {
+            if(a > b){
+                if(a > c) {
+                    return a;
+                } else {
+                    return c;
+                }
+            } else {
+                if(b > c) {
+                    return b;
+                } else {
+                    return c;
+                }
+            }
+        }
         public static void main(String[] args) {
 
             System.out.println("Welcome to Java Core - Generics Test Maximum.");
 
             System.out.println("Maximum from 3 Integer Object : "+compareTo(90,120,20));
+            System.out.println("Maximum from 3 Float Object : "+compareTo(90.0F,120.0F,190.0F));
         }
     }
